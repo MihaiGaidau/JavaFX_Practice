455b3dbf79b56764f819ec71824578d97aa71f33
MihaiGaidau <gaidaumihai@gmai.com>
2019-12-18 17:56:29 +0200

    just some staff

--- chapter02/src/sample/Main.java
+++ chapter02/src/sample/Main.java
@@ -2,6 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -15,10 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
+
         //create an Ellipse and set fill color
         Ellipse ellipse = new Ellipse(110, 70);
         ellipse.setFill(Color.rgb(173, 216, 230, .5));
@@ -29,7 +28,6 @@ public class Main extends Application {
 
         StackPane stackPane = new StackPane();
         stackPane.getChildren().addAll(ellipse, text);
-
         Scene scene = new Scene(stackPane, 350, 230, Color.LIGHTYELLOW);
         primaryStage.setTitle("MyShapes with JavaFX");
         primaryStage.setScene(scene);
